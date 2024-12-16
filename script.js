c23b24735aeeb4154ee8da7f43a3b0291fe45eaf
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-12-16 15:48:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+async function onSubmit(){
+let text = document.getElementById("text").value;
+let delay = parseInt(document.getElementById("delay").value, 10) 
+let output =  document.getElementById("output");
+	
+	if(!text || delay < 0 || isNaN(delay)){
+		alert("Enter the valid text and delay");
+		return;
+	}
+	
+await new Promise(resolve => setTimeout(resolve, delay)) ;	
+output.innerHTML = text; 
+}
